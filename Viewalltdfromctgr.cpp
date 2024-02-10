e5291519107466b48ce516fbca362d9f48b96300
bt9067 <anchanok_c@cmu.ac.th>
2024-02-10 23:29:28 +0700

    kk

--- Viewalltdfromctgr.cpp
+++ Viewalltdfromctgr.cpp
@@ -4,15 +4,17 @@
 using namespace std;
 
 void Viewalltdfromctgr(){
-    cout << "All task that is not complete.";
+    string notcompletetask;
+    cout << "What category do you want to view? : ";
+    cin >> notcompletetask; //ประเภทที่ยังไม่เสร็จทั้งหมด
+    cout << "This is all task that is not complete." << endl;
     ifstream task;
     task.open("notcomplete.txt");
     string textline;
     while(getline(task,textline)){
         cout << textline;
     }
-    task.close();
-    
+    task.close();    
 }
 
 int main(){
