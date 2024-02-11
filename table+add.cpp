ebcce6e8414a26af9872892435e3f3b889dec0ba
Pp <Pimolnat_ka@cmu.ac.th>
2024-02-12 00:11:03 +0700

    updated

--- table+add.cpp
+++ table+add.cpp
@@ -123,61 +123,69 @@ void getUserInput() {
 
     std::cout << "Add a Category? (y/n) : ";
     getline(cin, input);
-    while(input!="y" && input!="Y" && input!="n" && input!="N"){
+    while(true){
         cout << "Please enter only 'y' for yes or 'n' for no. (y/n) : ";
         getline(cin, input);
-    }
-    if(input=="y" || input == "Y"){
+    if(input == "yes" || input == "y" || input == "Y" || input == "Yes"){
     std::cout << "Enter Category: ";
     getline(cin, input);
     newEntry.push_back(input);
-    }else if(input=="n" || input == "N"){
+    break;
+    }else if(input == "no" || input == "n" || input == "N" || input == "No"){
         input = "No category";
         newEntry.push_back(input);
+        break;
+    }
     }
 
     std::cout << "Add a Due Date? (y/n) : ";
     getline(cin, input);
-    while(input!="y" && input!="Y" && input!="n" && input!="N"){
+    while(true){
         cout << "Please enter only 'y' for yes or 'n' for no. (y/n) : ";
         getline(cin, input);
-    }
-    if(input=="y" || input == "Y"){
+    if(input == "yes" || input == "y" || input == "Y" || input == "Yes"){
     std::cout << "Ender a Due Date : ";
     getline(cin, input);
     newEntry.push_back(input);
-    }else if(input=="n" || input == "N"){
+    break;
+    }else if(input == "no" || input == "n" || input == "N" || input == "No"){
         input = "No date";
         newEntry.push_back(input);
+        break;
+    }
     }
 
     std::cout << "Add a Remarks? (y/n) : ";
     getline(cin, input);
-    while(input!="y" && input!="Y" && input!="n" && input!="N"){
+    while(true){
         cout << "Please enter only 'y' for yes or 'n' for no. (y/n) : ";
         getline(cin, input);
-    }
-    if(input=="y" || input == "Y"){
+    if(input == "yes" || input == "y" || input == "Y" || input == "Yes"){
     std::cout << "Enter Remarks: ";
     getline(cin, input);
     newEntry.push_back(input);
-    }else if(input=="n" || input == "N"){
+    break;
+    }else if(input == "no" || input == "n" || input == "N" || input == "No"){
         input = "None";
         newEntry.push_back(input);
+        break;
+    }
     }
     
     std::cout << "Mark as important? (y/n) : ";
     getline(cin, input);
-    while(input!="y" && input!="Y" && input!="n" && input!="N"){
+    while(true){
         cout << "Please enter only 'y' for yes or 'n' for no. (y/n) : ";
         getline(cin, input);
-    }
-    if(input=="y" || input == "Y"){
+    if(input == "yes" || input == "y" || input == "Y" || input == "Yes"){
         input = "!";
         newEntry.push_back(input);
-    }else if(input=="n" || input == "N"){
+        break;
+    }else if(input == "no" || input == "n" || input == "N" || input == "No"){
         input = " ";
         newEntry.push_back(input);
+        break;
+    }
     }
 
     // Add the new entry to the data.csv file
