7310e61512cfb6f1c48ada1b8b961293773e67a8
bt9067 <anchanok_c@cmu.ac.th>
2024-02-13 23:39:38 +0700

    ยังไม่เคร

--- Viewalltdfromctgr.cpp
+++ Viewalltdfromctgr.cpp
@@ -3,22 +3,56 @@
 #include <fstream>
 using namespace std;
 
+void Category(){
+    system("cls");
+    int ans;
+
+    cout << "Category" << endl;
+    cout << "---------------------" << endl;
+    cout << "1. School" << endl;
+    cout << "2. Housework" << endl;
+    cout << "3. No category" << endl;
+    cout << "---------------------" << endl;
+    cout << "What category do you want to view? : ";   
+}
+void ReadData(){
+    ifstream read;
+    read.open("data.csv");
+}
 void Viewalltdfromctgr(){
-    string notcompletetask;
-    cout << "What category do you want to view? : ";
-    cin >> notcompletetask; //ประเภทที่ยังไม่เสร็จทั้งหมด
-    cout << "This is all task that is not complete." << endl;
-    ifstream task;
-    task.open("notcomplete.txt");
-    string textline;
-    while(getline(task,textline)){
-        cout << textline;
+    int ans;
+    do{ 
+        Category();
+        cin >> ans;
+        switch(ans){
+        case 1:
+                system("cls");
+                cout << "School" << endl;
+                ReadData();
+                break;
+            case 2:
+                system("cls");
+                cout << "Housework" << endl;
+                ReadData();
+                break;
+            case 3:
+                system("cls");
+                cout << "No category" << endl;
+                ReadData();
+                break;
+            default:
+                cout << "There is no this category. Please try again." << endl;
+                break;
     }
-    task.close();    
+    cin.ignore();
+    cin.get();
+    }while(ans != 3);   
 }
 
 int main(){
     Viewalltdfromctgr();
 }
+
+
     
 
