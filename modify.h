8b86216eee67d4589cc85724d4434b3fb3d1cb0b
Pp <gamerer16@gmail.com>
2024-02-28 01:22:56 +0700

    updated

--- modify.h
+++ modify.h
@@ -22,10 +22,17 @@ void modifyTodo(vector<map<string, string>>& data, const vector<string>& keys) {
     if (entryToUpdate != data.end()) {
         // Get user input for each field
         map<string, string> newEntry;
-        std::cout << "\033[1;32mEnter ID: ";
-        cout << "\033[1;37m";
+        while(newEntry.empty()){
+            cout << "\033[1;32mPlease enter a ID." << endl;
+            std::cout << "\033[1;32mEnter ID: ";
+            getline(cin, newEntry["ID"]);
+        }
+        
+    while (isDuplicateID(data, newEntry["ID"])) {
+        cout << "\033[1;31mID already exists. Please enter a new ID.\n";
+        cout << "\033[1;32mEnter ID: ";
         getline(cin, newEntry["ID"]);
-
+    }
         cout << "\033[1;32mEnter Todo: ";
         cout << "\033[1;37m";
         getline(cin, newEntry["Todo"]);
@@ -65,22 +72,79 @@ void modifyTodo(vector<map<string, string>>& data, const vector<string>& keys) {
         }while (true);
 
         cout << "\033[1;32mAdd a Due Date? (y/n): ";
-        do{
         cout << "\033[1;37m";
         getline(cin, input);
-        if (input == "y" || input == "Y" ) {
-            cout << "\033[1;32mEnter Due Date (y/m/d): ";
+        while (true) {
+        if (input == "yes" || input == "y" || input == "Y" || input == "Yes") {
+            // Ask whether to use today's date
+            cout << "\033[1;32mUse today's date? (y/n): ";
             cout << "\033[1;37m";
-            getline(cin, newEntry["Due Date"]);
-            break;
+            getline(cin, input);
+            while(true){
+                if(input == "yes" || input == "y" || input == "Y" || input == "Yes")
+                {
+                    /*std::cout << "\033[1;32mEnter Category: ";
+                    cout << "\033[1;37m";
+                    getline(cin, input);
+                    newEntry.push_back(input);*/
+                    break;
+                }
+                else if(input == "no" || input == "n" || input == "N" || input == "No")
+                {
+                    /*input = "No category";
+                    newEntry.push_back(input);*/
+                    break;
+                }
+                    cout << "\033[1;31mPlease enter only 'y' for yes or 'n' for no. (y/n) : ";
+                    cout << "\033[1;37m";
+                    getline(cin, input);
+            }
+
+            while (true) {
+                if (input == "yes" || input == "y" || input == "Y" || input == "Yes") {
+                    // Get today's date
+                    time_t now = time(0);
+                    tm* ltm = localtime(&now);
+                    string today = to_string(ltm->tm_mday) + "/" + to_string(1 + ltm->tm_mon) + "/" + to_string(1900 + ltm->tm_year);
+
+                    newEntry["Due Date"] = today;
+                    break;
+                } else if (input == "no" || input == "n" || input == "N" || input == "No") {
+                    while (true) 
+                    {
+                        // Get user input for date
+                        cout << "\033[1;32mEnter the due date (dd/mm/yyyy): ";
+                        cout << "\033[1;37m";
+                        getline(cin, input);
+                        // Validate user input for date format
+                        vector<string> dateParts = tokens(input, "/");
+                        if (dateParts.size() == 3) {
+                            int day = stoi(dateParts[0]);   
+                            int month = stoi(dateParts[1]);
+                            int year = stoi(dateParts[2]);
+                            if (year >= 1900 && year <= 9999 && month >= 1 && month <= 12 && day >= 1 && day <= 31) {
+                                newEntry["Due Date"] = input;
+                                break;
+                        }
+                    }
+                    cout << "\033[1;31mInvalid date format. Please enter date in dd/mm/yyyy format " << endl;
+                    } 
+                    break; 
+
+                }
+                cout << "\033[1;37m";
+                getline(cin, newEntry["Due Date"]);
+            } break;
         } else if (input == "no" || input == "n" || input == "N" || input == "No") {
-            newEntry["Due Date"] = "No date";
+            input = "No date";
+            newEntry["Due Date"] = input;
             break;
-        }else {
-        cout << "\033[1;31mInvalid input." << endl;
-        cout << "\033[1;32mAdd a Due Date? (y/n): ";
+        }
+        cout << "\033[1;31mPlease enter only 'y' for yes or 'n' for no. (y/n): ";
+        cout << "\033[1;37m";
+        getline(cin, newEntry["Due Date"]);
     }
-        }while (true);
+
 
         cout << "\033[1;32mAdd Remarks? (y/n): ";
         do{
