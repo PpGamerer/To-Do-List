44013aba4f62a0d78a7ae2c3722b65f3b3b38bfb
Pp <gamerer16@gmail.com>
2024-03-03 23:58:37 +0700

    updated

--- To-Do-List.cpp
+++ To-Do-List.cpp
@@ -33,7 +33,7 @@ int main() {
         row = tokens(textline, ",");  //delimiter = ","
         myMap.clear(); //เคลียร์mapก่อนpair
         for (size_t i = 0; i < keys.size(); i++) { //ทำจนครบทุกคอลัมน์
-            myMap.insert(pair<string, string>(keys.at(i), row[i])); //ใส่ค่า row[i] ลงคู่กับ keys.at(i) ตามคอลัมน์
+            myMap[keys.at(i)] = row[i]; //ใส่ค่า row[i] ลงคู่กับ keys.at(i) ตามคอลัมน์
         }
         data.push_back(myMap); //ใส่ทุกคู่ลงใน data
     }
